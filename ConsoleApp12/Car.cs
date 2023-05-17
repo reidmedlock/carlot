3185662ae77459659a9238ea29b44a568bb7c454
reidmedlock <benmedlock19@gmail.com>
2023-05-17 19:12:12 -0400

    Add project files.

---
+++ ConsoleApp12/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Car
+    {
+        public string Make { get; set; }  
+        public string Model { get; set; }   
+        public int Year { get; set; }
+
+         public decimal Price { get; set; }
+        public Car()
+        {
+            Make = "";
+            Model = "";
+            Year = "";
+            Price = "";
+
+
+
+
+        }
+
+
+
+        public Car(string carMake, string carModel, int carYear, decimal carPrice)
+        {
+            Make = carMake;
+            Model = carModel;   
+            Year = carYear; 
+            Price = carPrice;  
+
+
+        }
+
+        
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp12;
+
+Car car1 = new Car("Lamborghini", "Murcielago", 2005, 250000 );
+Car car2 = new Car("Ferrari", "FF", 2012, 140000);
+Car car3 = new Car("")
